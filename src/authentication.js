0947fb4eeea1eb03b030ff8e5d5a1d5917d67253
imaidev2021 <90123609+imaidev2021@users.noreply.github.com>
2021-09-20 17:34:30 +0900

    add3
    add3

--- src/authentication.js
+++ src/authentication.js
@@ -4,7 +4,7 @@
  */
 
  
-var VALID_USERNAME = process.env.USER_NAME || "jbfree5";
+var VALID_USERNAME = process.env.USER_NAME || "";
 
 
 exports.isOwnerValid = function (queryObject) {
--- test/authentication_test.js
+++ test/authentication_test.js
@@ -12,7 +12,6 @@ var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
 var shouldRejected  = require("promise-test-helper").shouldRejected;
 
 var target = require("../src/authentication.js");
-const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 
 
@@ -33,9 +32,8 @@ describe( "authentication.js", function(){
         // ここからテスト。
         it("パスワードが正しい場合", function(){
             var isOwnerValid = target.isOwnerValid;
-            var getQuery = {"pass":"ss1met1j"};
+            var getQuery = {"pass":""};
             var result = isOwnerValid( getQuery );
-            await _sleep(2000);
             assert( result, "trueが返却されること" )
         });
         it("パスワードが不正の場合");
