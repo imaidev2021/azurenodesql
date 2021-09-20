27591fd64f5e7568141255cff007494e8e67c7bb
imaidev2021 <90123609+imaidev2021@users.noreply.github.com>
2021-09-20 17:13:27 +0900

    add2
    add2

--- test/authentication_test.js
+++ test/authentication_test.js
@@ -12,6 +12,9 @@ var shouldFulfilled = require("promise-test-helper").shouldFulfilled;
 var shouldRejected  = require("promise-test-helper").shouldRejected;
 
 var target = require("../src/authentication.js");
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+
 
 
 describe( "authentication.js", function(){
@@ -32,7 +35,7 @@ describe( "authentication.js", function(){
             var isOwnerValid = target.isOwnerValid;
             var getQuery = {"pass":"ss1met1j"};
             var result = isOwnerValid( getQuery );
-
+            await _sleep(2000);
             assert( result, "trueが返却されること" )
         });
         it("パスワードが不正の場合");
